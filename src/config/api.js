b4424c446dea013b1b34d86e6312e316493129ee
lissavxo <lissavitoria12@gmail.com>
2022-01-28 09:02:32 -0300

    build: new release builded

--- src/config/api.js
+++ src/config/api.js
@@ -2,6 +2,6 @@ const {VUE_APP_API_BASE_URL,
     VUE_APP_API_PORT} = process.env
 
 module.exports = {
-    baseUrl: "http://localhost",
+    baseUrl: VUE_APP_API_BASE_URL,
     port: VUE_APP_API_PORT
 }
